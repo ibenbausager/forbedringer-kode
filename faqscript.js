038b9feba007b506369d7deb82e6f048086d2ebd
LaerkeSoeby <laer9384@edu.zealand.dk>
2022-06-17 11:39:13 +0200

    Javascripts kommentar

--- faqscript.js
+++ faqscript.js
@@ -4,12 +4,13 @@ var acc = document.getElementsByClassName("spørgsmål");
 var i;
 
 for (i = 0; i < acc.length; i++) {
-    acc[i].addEventListener("click", function() {
-    this.classList.toggle("aktiv");
+    //i=0 fordi den skal starte med 0 og så tilføje 1 og derefter 1 (dvs bliver til 2) - pga i++. (hhv. nr 1, 2, 3 osv boks)
+    acc[i].addEventListener("click", function() { //function burde stå i en linie under
+        this.classList.toggle("aktiv");
 
-    var svar = this.nextElementSibling;
-    if (svar.style.display === "block") {
-        svar.style.display = "none";
+        var svar = this.nextElementSibling;
+        if (svar.style.display === "block") {
+            svar.style.display = "none";
         } else {
             svar.style.display = "block";
         }
